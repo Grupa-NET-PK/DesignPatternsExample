ee2497ebc3ed14690050c48904c520af96ace20c
YourSenseiCreeper <tomanycreeps@gmail.com>
2021-09-09 20:45:04 +0200

    Taki extension builder

---
+++ ExtensionBuilder/Man.cs
@@ -0,0 +1,45 @@
+﻿namespace Builder
+{
+	public class Man
+    {
+        public string Name { get; set; }
+
+        public double Fame { get; set; }
+
+        public double Inteligence { get; set; }
+
+        public double Money { get; set; }
+
+        public double Stamina { get; set; }
+
+        public Man AddFame(double fame)
+        {
+            Fame = fame;
+            return this;
+        }
+
+        public Man AddInteligence(double inteligence)
+        {
+            Inteligence = inteligence;
+            return this;
+        }
+
+        public Man AddMoney(double money)
+        {
+            Money = money;
+            return this;
+        }
+
+        public Man AddStamina(double stamina)
+        {
+            Stamina = stamina;
+            return this;
+        }
+
+        public Man AddName(string name)
+        {
+            Name = name;
+            return this;
+        }
+    }
+}
---
+++ ExtensionBuilder/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var chad = new Man()
+                .AddName("Chad")
+                .AddFame(100)
+                .AddMoney(100);
+            var virgin = new Man()
+                .AddName("Virgin")
+                .AddFame(0)
+                .AddMoney(0);
+
+            Man[] manList = { chad, virgin };
+
+            foreach (var man in manList)
+            {
+                Console.WriteLine($"Man name: {man.Name}");
+                Console.WriteLine($"Man fame: {man.Fame}");
+                Console.WriteLine($"Man inteligence: {man.Inteligence}");
+                Console.WriteLine($"Man stamina: {man.Stamina}");
+                Console.WriteLine($"Man money: {man.Money}");
+                Console.WriteLine(new string('-', 10));
+            }
+
+
+        }
+    }
+}
